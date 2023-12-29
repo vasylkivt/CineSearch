44528fdb84c44985cc60dcd614052ffefd85dd2c
vasylkivt <vasylkiv.t@gmail.com>
2023-12-29 12:51:02 +0200

    delete unnecessary files;

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="goit-react-hw-05-movies">
+    <BrowserRouter basename="/">
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <App />
